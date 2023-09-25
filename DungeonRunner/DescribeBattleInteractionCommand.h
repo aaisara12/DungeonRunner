b4be21373d0fac003d6e0ca483ceb8331b3cc006
Aaron Isara <68300841+aaisara12@users.noreply.github.com>
2023-09-25 12:05:00 -0700

    Used format library to inject character names into battle text

--- DungeonRunner/DescribeBattleInteractionCommand.h
+++ DungeonRunner/DescribeBattleInteractionCommand.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Command.h"
+#include "Character.h"
+#include <format>
 class DescribeBattleInteractionCommand : public Command
 {
 public:
@@ -10,8 +12,15 @@ public:
 	// Inherited via Command
 	virtual std::list<Command*> Execute(ObservableVariable<std::string>& textOutput) override
 	{
-		// TODO: Apply formatting to the text to insert source and target names
-		textOutput.set(battleInteraction.move->actionText);
+		// We assume actionText has formatting characters to inject the combatant names
+		std::string completeActionText = 
+			std::vformat(
+				battleInteraction.move->actionText, 
+				std::make_format_args(
+					battleInteraction.source->getName(), 
+					battleInteraction.target->getName()));
+
+		textOutput.set(completeActionText);
 		return std::list<Command*>();
 	}
 
--- DungeonRunner/DummyCharacterLoader.h
+++ DungeonRunner/DummyCharacterLoader.h
@@ -5,9 +5,9 @@ class DummyCharacterLoader : public CharacterLoader
 public:
 	virtual std::list<Character*> getCharacters()
 	{
-		BattleMove* whiffMove = new BattleMove("Whiff", 100, 0, 0.0f, 0, "attempts to throw a punch", BattleMove::ENEMY);
-		BattleMove* punchMove = new BattleMove("Punch", 10, 0, 1.0f, 1, "throws a fist!", BattleMove::ENEMY);
-		BattleMove* healMove = new BattleMove("Heal", 0, 20, 1, 2, "glows with purity!", BattleMove::ALLY);
+		BattleMove* whiffMove = new BattleMove("Whiff", 100, 0, 0.0f, 0, "{0} attempts to throw a punch at {1}", BattleMove::ENEMY);
+		BattleMove* punchMove = new BattleMove("Punch", 10, 0, 1.0f, 1, "{0} throws a fist at {1}!", BattleMove::ENEMY);
+		BattleMove* healMove = new BattleMove("Heal", 0, 20, 1, 2, "{0} glows with purity!", BattleMove::ALLY);
 
 		std::vector<BattleMove*> moveset1 = { punchMove, healMove };
 		std::vector<BattleMove*> moveset2 = { whiffMove, healMove };
