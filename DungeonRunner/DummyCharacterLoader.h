4e6a00b5bffd2b3acec7317c3d9597b06c93f30b
Aaron Isara <68300841+aaisara12@users.noreply.github.com>
2023-09-26 01:39:24 -0700

    Modified dummy character stats to be more interesting

--- DungeonRunner/DummyCharacterLoader.h
+++ DungeonRunner/DummyCharacterLoader.h
@@ -5,13 +5,30 @@ class DummyCharacterLoader : public CharacterLoader
 public:
 	virtual std::list<Character*> getCharacters()
 	{
-		BattleMove* whiffMove = new BattleMove("Whiff", 100, 0, 0.0f, 0, "{0} attempts to throw a punch at {1}", BattleMove::ENEMY);
+		BattleMove* whiffMove = new BattleMove("Kick", 25, 0, 1.0f, 0, "{0} kicks {1} in the shins!", BattleMove::ENEMY);
 		BattleMove* punchMove = new BattleMove("Punch", 10, 0, 1.0f, 1, "{0} throws a fist at {1}!", BattleMove::ENEMY);
-		BattleMove* healMove = new BattleMove("Heal", 0, 20, 1, 2, "{0} glows with purity!", BattleMove::ALLY);
+		BattleMove* healMove = new BattleMove("Heal", 0, 4, 1, 2, "{0} glows with purity!", BattleMove::ALLY);
 
 		std::vector<BattleMove*> moveset1 = { punchMove, healMove };
 		std::vector<BattleMove*> moveset2 = { whiffMove, healMove };
 
-		return std::list<Character*>{new Character("Anduin", moveset1), new Character("Gul'dan", moveset2)};
+		std::unordered_map<Character::StatType, int> stats1
+		{
+			{Character::CUR_HP, 100},
+			{Character::MAX_HP, 100},
+			{Character::DEF, 1},
+			{Character::ATK, 1},
+			{Character::HEAL, 3}
+		};
+		std::unordered_map<Character::StatType, int> stats2
+		{
+			{Character::CUR_HP, 40},
+			{Character::MAX_HP, 40},
+			{Character::DEF, 0},
+			{Character::ATK, 4},
+			{Character::HEAL, 2}
+		};
+
+		return std::list<Character*>{new Character("Anduin", moveset1, stats1), new Character("Gul'dan", moveset2, stats2)};
 	}
 };
